2a8db2c44069341459e547f6894ce506bdfb31f3
RahulKoju <blcraz75@gmail.com>
2024-07-24 16:14:20 +0545

    Add: Middlewares

--- api/index.js
+++ api/index.js
@@ -3,7 +3,10 @@ dotenv.config();
 import express from "express";
 import mongoose from "mongoose";
 import authRoutes from "./routes/auth.routes.js";
+import { errorMiddleware } from "./middlewares/error.middleware.js";
 const PORT = process.env.PORT || 8000;
+
+//Connection
 mongoose
   .connect(process.env.MONGODB_URL)
   .then(console.log("MongoDB is connected!!"))
@@ -15,8 +18,12 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/api/auth",authRoutes);
+//Routes
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server started at port:${PORT}`);
 });
+
+//Middlewares
+app.use(errorMiddleware);
