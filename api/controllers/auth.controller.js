57f1a430f2df468caa9e5e322d1177428f9887b5
RahulKoju <blcraz75@gmail.com>
2024-07-24 16:16:17 +0545

    Add: Function to handle errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
-export const handleSignUp= async(req, res, next) => {
+import { errorHandler } from "../utils/error.utils.js";
+export const handleSignUp = async (req, res, next) => {
   const { fullname, email, password } = req.body;
   if (
     !fullname ||
@@ -9,8 +10,13 @@ export const handleSignUp= async(req, res, next) => {
     email === "" ||
     password === ""
   ) {
-    return res.status(400).json({ message: "All feilds are required" });
+    next(errorHandler(400, "All feilds are required"));
   }
   const newUser = new User({ fullname, email, password });
-  await newUser.save();
-}
+  try {
+    await newUser.save();
+    res.json("signin successfull");
+  } catch (error) {
+    next(error);
+  }
+};
