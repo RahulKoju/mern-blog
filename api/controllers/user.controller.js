fac774c3b8e7f1186214acb6ef2a53208c57a8d0
RahulKoju <blcraz75@gmail.com>
2024-07-31 22:42:51 +0545

    Fix: UpdateUser API for hashing password

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,42 +1,49 @@
 import { errorHandler } from "../utils/error.utils.js";
 import User from "../models/user.model.js";
+
 export const updateUser = async (req, res, next) => {
   const { profilePicture, fullname, email, password } = req.body;
-  console.log(req.body);
+
   if (req.user._id !== req.params.userId) {
-    return next(errorHandler(403, "You are not allowed to update this user"));
+    return next(errorHandler(403, "You are not allowed to update this user")); 
   }
-  if (password) {
-    if (password.length < 8) {
-      return next(errorHandler(400, "Password must be at least 8 characters"));
-    }
-  }
-  if (fullname) {
-    if (fullname.length < 7 || fullname.length > 20) {
-      return next(
-        errorHandler(400, "Fullname must be between 7 to 20 characters")
-      );
-    }
-    if (!fullname.match(/^[a-zA-Z0-9]+$/)) {
-      return next(
-        errorHandler(400, "Fullname can only contain letters and numbers")
-      );
-    }
-  }
+
   try {
-    const updatedUser = await User.findByIdAndUpdate(
-      req.params.userId,
-      {
-        $set: {
-          fullname: fullname,
-          email: email,
-          password: password,
-          profilePicture: profilePicture,
-        },
-      },
-      { new: true }
-    );
-    const { excludedPassword, ...rest } = updatedUser._doc;
+    const user = await User.findById(req.params.userId);
+    if (!user) {
+      return next(errorHandler(404, "User not found"));
+    }
+
+    if (password) {
+      if (password.length < 8) {
+        return next(
+          errorHandler(400, "Password must be at least 8 characters")
+        );
+      }
+      user.password = password;
+    }
+
+    if (fullname) {
+      if (fullname.length < 7 || fullname.length > 20) {
+        return next(
+          errorHandler(400, "Fullname must be between 7 to 20 characters")
+        );
+      }
+      if (!fullname.match(/^[a-zA-Z0-9]+$/)) {
+        return next(
+          errorHandler(400, "Fullname can only contain letters and numbers")
+        );
+      }
+      user.fullname = fullname;
+    }
+    if (email) {
+      user.email = email;
+    }
+    if (profilePicture) {
+      user.profilePicture = profilePicture;
+    }
+    const updatedUser = await user.save();
+    const { password: _, ...rest } = updatedUser._doc;
     res.status(200).json(rest);
   } catch (error) {
     next(error);
