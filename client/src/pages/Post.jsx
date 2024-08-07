654e2c0938312607e1e66f1b5a25331fef7f722f
RahulKoju <blcraz75@gmail.com>
2024-08-07 14:18:46 +0545

    Add: Comment section to the post page

--- client/src/pages/Post.jsx
+++ client/src/pages/Post.jsx
@@ -2,6 +2,7 @@ import { Button, Spinner } from "flowbite-react";
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import CallToAction from "../components/CallToAction";
+import CommentSection from "../components/CommentSection";
 export default function Post() {
   const { postSlug } = useParams();
   const [loading, setLoading] = useState(true);
@@ -63,6 +64,7 @@ export default function Post() {
       <div className="max-w-4xl mx-auto w-full">
         <CallToAction/>
       </div>
+      <CommentSection postId={post._id}/>
     </main>
   );
 }
