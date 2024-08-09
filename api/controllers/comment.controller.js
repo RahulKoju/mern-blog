ba343f45d12b1580840d3fd8ae02ec2c688ffcd4
RahulKoju <blcraz75@gmail.com>
2024-08-09 13:19:27 +0545

    Add: Edit comment API route

--- api/controllers/comment.controller.js
+++ api/controllers/comment.controller.js
@@ -36,7 +36,7 @@ export const likeComment = async (req, res, next) => {
   try {
     const comment = await Comment.findById(req.params.commentId);
     if (!comment) {
-      return next(errorHandler(404, 'Comment not found'));
+      return next(errorHandler(404, "Comment not found"));
     }
     const userIndex = comment.likes.indexOf(req.user._id);
     if (userIndex === -1) {
@@ -46,6 +46,25 @@ export const likeComment = async (req, res, next) => {
       comment.numberOfLikes -= 1;
       comment.likes.splice(userIndex, 1);
     }
+    await comment.save();
+    res.status(200).json(comment);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const editComment = async (req, res, next) => {
+  try {
+    const comment = await Comment.findById(req.params.commentId);
+    if (!comment) {
+      return next(errorHandler(404, "Comment not found"));
+    }
+    if (comment.userId !== req.user._id && !req.user.isAdmin) {
+      return next(
+        errorHandler(403, "You are not allowed to edit this comment")
+      );
+    }
+    comment.content = req.body.content;
     await comment.save();
     res.status(200).json(comment);
   } catch (error) {
