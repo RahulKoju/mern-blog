61beeb96a723765bc8fa9ef9b13009bbcac56414
RahulKoju <blcraz75@gmail.com>
2024-08-13 14:47:35 +0545

    Add: Token model for resetting a password given a token

---
+++ api/models/token.model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const tokenSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  token: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now(),
+    expires: 3600, //1hr
+  },
+});
+
+const Token = mongoose.model("Token", tokenSchema);
+export default Token;
