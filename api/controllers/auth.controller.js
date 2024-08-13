a013e68e587baf33ec622bae9b329462821cc7b4
RahulKoju <blcraz75@gmail.com>
2024-08-13 14:49:14 +0545

    Add: Forgot and Reset password API route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,9 @@
+import Token from "../models/token.model.js";
 import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.utils.js";
+import crypto from "crypto";
+import { sendEmail } from "../utils/sendEmail.utils.js";
+
 export const handleSignUp = async (req, res, next) => {
   const { fullname, email, password } = req.body;
   if (
@@ -20,6 +24,7 @@ export const handleSignUp = async (req, res, next) => {
     next(error);
   }
 };
+
 export const handleSignIn = async (req, res, next) => {
   const { email, password } = req.body;
   if (!email || !password || email === "" || password === "") {
@@ -44,12 +49,66 @@ export const handleSignIn = async (req, res, next) => {
     );
   }
 };
+
 export const handleSignOut = (req, res, next) => {
   try {
     res
       .clearCookie("Access_token")
       .status(200)
       .json({ message: "User has been signed out" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const handleForgotPassword = async (req, res, next) => {
+  const { email } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ message: "User with this email does not exist." });
+    }
+    let token = await Token.findOne({ userId: user._id });
+    if (!token) {
+      token = new Token({
+        userId: user._id,
+        token: crypto.randomBytes(32).toString("hex"),
+      });
+      await token.save();
+    }
+    const resetUrl = `${process.env.CLIENT_URL}/reset-password/${token.token}`;
+    const text = `<p>You requested a password reset. Click the link below to reset your password:</p>
+        <a href="${resetUrl}">${resetUrl}</a>
+        <p>If you didn't request this, please ignore this email.</p>`;
+    await sendEmail(user.email, "Password Reset", text);
+    res
+      .status(200)
+      .json({ message: "Password reset link sent to your email." });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const handleResetPassword = async (req, res, next) => {
+  const { token } = req.params;
+  const { password } = req.body;
+  try {
+    const resetToken = await Token.findOne({ token });
+    if (!resetToken) {
+      return res.status(400).json({ message: "Invalid or expired token" });
+    }
+    const user = await User.findById(resetToken.userId);
+    if (!user) {
+      return res
+        .status(400)
+        .json({ message: "User not found or Invalid Link" });
+    }
+    user.password = password;
+    await user.save();
+    await resetToken.deleteOne();
+    res.status(200).json({ message: "Password has been reset." });
   } catch (error) {
     next(error);
   }
--- api/routes/auth.routes.js
+++ api/routes/auth.routes.js
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import {
+  handleForgotPassword,
+  handleResetPassword,
   handleSignIn,
   handleSignOut,
   handleSignUp,
@@ -9,4 +11,6 @@ const router = Router();
 router.post("/signup", handleSignUp);
 router.post("/signin", handleSignIn);
 router.post("/signout", handleSignOut);
+router.post("/forgot-password", handleForgotPassword);
+router.post("/reset-password/:token", handleResetPassword);
 export default router;
